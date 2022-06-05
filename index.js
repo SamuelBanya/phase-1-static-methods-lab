9fe58c80147fee802f856f7cc9e5058690111e65
Samuel Banya <sbanya@fastmail.com>
2022-06-05 18:14:12 -0400

    commit tree d8d5aedb6cf7491044045059897b3dc195a968c9

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  constructor() {
+    this.word = word;
+    this.sentence = sentence;
+  }
+  static capitalize(word) {
+    return (word[0] + word[1, word.length]).toString();
+  }
+
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9]+/g, "");
+  }
+
+  static titleize(sentence) {
+    sentence = this.sentence.split(" ");
+    let titleizeSentenceArray = [];
+    let avoidWordsArray = ["a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    sentence.forEach((word) => {
+      if (!word in avoidWordsArray) {
+        let uppercaseWord = word[0].toUpperCase() + word[1, word.length];
+        titleizeSentenceArray.push(uppercaseWord);
+        titleizeSentenceArray.push(" ");
+      } else {
+        titleizeSentenceArray.push(word);
+        titleizeSentenceArray.push(" ");
+      }
+    });
+
+    console.log(`titleizeSentenceArray: ${titleizeSentenceArray}`);
+
+    return titleizeSentenceArray;
+  }
 }
